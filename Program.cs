1ac88917e06bce1b769b40d53bf3be3701b04305
Vik-11 <vikkuznetsov11@gmail.com>
2025-04-16 07:02:44 +0500

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -4,36 +4,36 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        //Console.WriteLine("Enter path to file with extension .txt for Ex1");
-        //string path1 = Console.ReadLine();
-        //TextFile ex1 = new TextFile(path1);
-        //ex1.RunEx1();
-        //Console.WriteLine();
+        Console.WriteLine("Enter path to file with extension .txt for Ex1");
+        string path1 = Console.ReadLine();
+        TextFile ex1 = new TextFile(path1);
+        ex1.RunEx1();
+        Console.WriteLine();
 
-        //Console.WriteLine("Enter path to file with extension .txt for Ex2");
-        //string path2 = Console.ReadLine();
-        //TextFile ex2 = new TextFile(path2);
-        //ex2.RunEx2();
-        //Console.WriteLine();
+        Console.WriteLine("Enter path to file with extension .txt for Ex2");
+        string path2 = Console.ReadLine();
+        TextFile ex2 = new TextFile(path2);
+        ex2.RunEx2();
+        Console.WriteLine();
 
-        //Console.WriteLine("Enter path to output file with extension .txt for Ex3");
-        //string path3 = Console.ReadLine();
-        //TextFile ex3 = new TextFile(path3);
-        //ex3.RunEx3();
+        Console.WriteLine("Enter path to output file with extension .txt for Ex3");
+        string path3 = Console.ReadLine();
+        TextFile ex3 = new TextFile(path3);
+        ex3.RunEx3();
 
-        //Console.WriteLine("Enter path to file with extension .txt for Ex4");
-        //string path4 = Console.ReadLine();
-        //TextFile ex4 = new TextFile(path4);
-        //ex4.RunEx4();
+        Console.WriteLine("Enter path to file with extension .txt for Ex4");
+        string path4 = Console.ReadLine();
+        TextFile ex4 = new TextFile(path4);
+        ex4.RunEx4();
 
-        //TextFile ex5 = new TextFile();
-        //ex5.RunEx5();
+        TextFile ex5 = new TextFile();
+        ex5.RunEx5();
 
-        //TextFile ex6 = new TextFile();
-        //ex6.RunEx6();
+        TextFile ex6 = new TextFile();
+        ex6.RunEx6();
 
-        //TextFile ex7 = new TextFile();
-        //ex7.RunEx7();
+        TextFile ex7 = new TextFile();
+        ex7.RunEx7();
 
         TextFile ex8 = new TextFile();
         ex8.RunEx8();
